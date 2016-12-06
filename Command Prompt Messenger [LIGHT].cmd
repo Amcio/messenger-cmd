8f48300c6059a24f3781ef9600f908f7e2ea8f20
Amcio <amcio122@gmail.com>
2016-12-06 21:19:41 +0100

    Version 1.3 Beta And Light Version Beta Started

---
+++ Command Prompt Messenger [LIGHT].cmd
@@ -0,0 +1,34 @@
+@echo off
+Cls
+:: I don't care if you want to copy this and public it as your own. Really. Do what you want with it.
+:: BUT if you make an update i want to check it out so if you do sent it to me amcio122@gmail.com
+:: I hope you will find this cool to use or scare your friends while they play CS:GO
+:: Oh and look for my updates on GitHub.
+color 08
+title Command Prompt Messenger By Amcio [LIGHT]
+echo ==================================MESSENGER=====================================
+echo Program created by: Amcio
+echo Information: Type IP/Computer Name to connect to another computer.
+echo If you want to send message to someone else type: restart in the message
+echo -----------------------------------
+:Start
+cls
+echo ==================================MESSENGER=====================================
+echo Program created by: Amcio
+echo Information: Type IP/Computer Name to connect to another computer.
+echo If you want to send message to someone else type: restart in the message
+echo --------------------
+set /p n=Computer Name/IP:
+cls
+:A
+echo ==================================MESSENGER=====================================
+echo Program created by: Amcio
+echo Information: Type IP/Computer Name to connect to another computer.
+echo If you want to send message to someone else type: restart in the message
+echo --------------------
+set /p m=Message:
+if %m% EQU restart goto :Start
+msg /server:%n% * "%m%"
+Pause
+cls
+Goto A
